bcccfdf783066acc6ace35828aa27db21a79a838
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-07-07 13:35:20 +0100

    Sami Saeed

--- src/token.js
+++ src/token.js
@@ -1,34 +1,39 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 /**
  * Implement this function to accept a payload and a secret key and return a JWT without an expiry time
- * 
+ *
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
  */
-function createToken() {
-    
+function createToken(payload, secretKey) {
+  return jwt.sign(payload, secretKey);
 }
 
 /**
  * Implement this function to accept a payload, secret key and an expiry time, and return a JWT with an expiry
- * 
+ *
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#token-expiration-exp-claim
  */
-function createTokenWithExpiry() {
-    
+function createTokenWithExpiry(payload, secretKey, expiryTime) {
+  return jwt.sign(payload, secretKey, { expiresIn: expiryTime });
 }
 
 /**
  * Implement this function to accept a JWT and a secret key. Return the decoded token (the payload) if verification is successful, and false if it fails
- * 
+ *
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
  */
-function verifyToken() {
-
+function verifyToken(token, secretKey) {
+  try {
+    return jwt.verify(token, secretKey);
+  } catch (e) {
+    console.error("error");
+    return false;
+  }
 }
 
 module.exports = {
-    createToken,
-    createTokenWithExpiry,
-    verifyToken
-}
+  createToken,
+  createTokenWithExpiry,
+  verifyToken,
+};
